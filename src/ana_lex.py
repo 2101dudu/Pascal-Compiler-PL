c2ffbdfbce519686bc6eac96f550e20aa1322f04
Eduardo Faria <eduardo21faria@gmail.com>
2025-04-02 16:27:24 +0100

    feat: add some stuff

--- src/ana_lex.py
+++ src/ana_lex.py
@@ -7,35 +7,44 @@ tokens = (
     'id', 'string'
 )
 
+
 def t_PROGRAM(t):
     r'program'
     return t
+
 
 def t_BEGIN(t):
     r'begin'
     return t
 
+
 def t_END(t):
     r'end'
     return t
 
+
 def t_WRITELN(t):
     r'writeln'
     return t
+
 
 def t_string(t):
     r'\'[^\']*?\''
     t.value = t.value[1:-1]
     return t
 
+
 def t_id(t):
     r'[a-zA-Z_][a-zA-Z0-9_]*'
-    return t  
+    return t
+
 
 t_ignore = ' \t\n'
+
 
 def t_error(t):
     print(f"Caractere inválido: {t.value[0]}")
     t.lexer.skip(1)
+
 
 lexer = lex.lex()
--- src/ana_sin.py
+++ src/ana_sin.py
@@ -1,22 +1,26 @@
+import sys
 import ply.yacc as yacc
 from lexer import tokens
 from gen import gen_code
+
 
 def p_Program(p):
     "Program : PROGRAM id ';' BEGIN Statement END '.'"
     p[0] = ("program", p[2], p[5])
 
+
 def p_Statement_writeln(p):
     "Statement : WRITELN '(' string ')' ';'"
     p[0] = ("writeln", p[3])
+
 
 def p_error(p):
     parser.success = False
     print("Erro de sintaxe!")
 
+
 parser = yacc.yacc()
 
-import sys
 text = sys.stdin.read()
 parser.success = True
 parsed = parser.parse(text)
