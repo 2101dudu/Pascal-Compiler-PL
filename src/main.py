0d25619658e7214f6175689f08f6e604d1070f72
Eduardo Faria <eduardo21faria@gmail.com>
2025-05-31 20:36:44 +0100

    feat: add semantic pass to validate code quality

--- src/main.py
+++ src/main.py
@@ -2,10 +2,12 @@ import sys
 
 from gen import gen_code
 from ana_sin import parse_pascal
+from ana_sem import SemanticAnalyzer
 
 
 if __name__ == "__main__":
     texto = sys.stdin.read()
-    result = parse_pascal(texto)
-    # print(f"\nTree:\n{result}\n\n")
-    print(f"{gen_code(result)}")
+    ast_root = parse_pascal(texto)
+    SemanticAnalyzer().analyze(ast_root)
+    # print(f"\nTree:\n{ast_root}\n\n")
+    print(f"{gen_code(ast_root)}")
